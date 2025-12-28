1ce380997b0f6c2835d7c3755c96f81350b86101
sourovroy999 <sourovroy3050@gmail.com>
2025-12-28 21:30:04 +0600

    horizontail scroll triggering secondary animation

--- src/main.js
+++ src/main.js
@@ -5,31 +5,75 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger)
 
 
-
-
 const races = document.querySelector(".races");
-// console.log('offsetwidth',races.offsetWidth)
 
-function getScrollAmount(){
-  let racesScrollWidth=races.scrollWidth;
-  return -(racesScrollWidth-window.innerWidth)
+function getScrollAmount() {
+  let racesScrollWidth = races.scrollWidth;
+  return -(racesScrollWidth - window.innerWidth )
 }
 
-const tween=gsap.to(races, {
- x:getScrollAmount,
- duration:3,
- ease:'none'  
+const tween = gsap.fromTo(races, {
+
+  x: () => window.innerWidth
+
+}, {
+  x: getScrollAmount,
+  duration: 3,
+  ease: 'none'
 })
 
 ScrollTrigger.create({
-  trigger:'.racesWrapper',
-  start:'top 20%',
-  end:()=> `+=${getScrollAmount() * -1}`,
-  pin:true,
-  animation:tween,
-  scrub:1,
-  invalidateOnRefresh:true,
-  markers:true
+  trigger: '.racesWrapper',
+  start: 'top 20%',
+  end: () => `+=${getScrollAmount() * -1}`,
+  pin: true,
+  animation: tween,
+  scrub: 1,
+  invalidateOnRefresh: true,
+  markers: true
 })
+
+const sections = gsap.utils.toArray(".races div")
+const maps=gsap.utils.toArray('.map')
+
+sections.forEach((section, index) => {
+
+  const map=maps[index]
+  const mapsAni=gsap.timeline({paused:true})
+  .from(map, {scale:0.5, rotation:-20})
+  .to(map, {opacity:1}, 0)
+
+  ScrollTrigger.create({
+    trigger: section,
+    containerAnimation: tween,
+    start: 'left 50%',
+    animation: gsap.to(section.querySelector("h2"), { scale: 0.6, }),
+    
+    end:'right 50%',
+    
+    scrub: true,
+    markers:true,
+    
+    // onEnter: () => mapsAni.play(),
+    // onLeave:()=> mapsAni.pause(0),
+    // onEnterBack: () => mapsAni.play(),
+    // onLeaveBack:()=> mapsAni.pause(0),
+
+    // onToggle:(self)=>{
+    //   if(self.isActive){
+    //     mapsAni.play()
+    //   }else{
+    //     mapsAni.pause(0)
+    //   }
+
+    onToggle:(self)=> self.isActive? mapsAni.play() : mapsAni.pause(0)
+    }
+
+
+
+  })
+})
+
+
 
 
